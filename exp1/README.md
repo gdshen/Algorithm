6b788ed41dee21388b88534d34960640651865f0
gdshen <gdshen95@gmail.com>
2015-04-06 23:30:25 +0800

    complete quick sort alg

--- exp1/README.md
+++ exp1/README.md
@@ -8,8 +8,12 @@ arrow.now()
 ## 随机数生成模块
 ### numpy中的随机数生成模块
 import numpy as np
+
 np.random.random(size=10) 参数为数组的大小
+
 np.random.rand(d0,d1,...) 参数为各维度的数值
+
 np.random.randn(d0,d1,...) 同上，返回的是正态分布的数值
-返回的都是numpy的数组
+
+所有的返回的都是numpy的数组
 
--- exp1/quicksort.py
+++ exp1/quicksort.py
@@ -1,20 +1,42 @@
-from pprint import pprint
 import numpy as np
-import arrow  # 显示时间
-import timeit
+import time
 
 
-def quicksort(start, end, mid):
-    '''
-    Simple description about this function
-    :param start: fe
-    :param end:
-    :param mid:
-    :return:
-    '''
-    print('hello,world')
+def partition(array, start, end):
+    """
+    the partition process of quicksort
+    :param array: the array to be sorted
+    :param start: the start position
+    :param end: the end position
+    :return: the flag element position
+    """
+    x = array[end]
+    i = start - 1
+    for j in range(start, end):
+        if array[j] <= x:
+            i += 1
+            array[i], array[j] = array[j], array[i]
+    array[i + 1], array[end] = array[end], array[i + 1]
+    return i + 1
+
+
+def quicksort(array, start, end):
+    """
+    This is the quick sort algorithm.
+    :param array: the sub array which we want to sort
+    :param start: the start position of the sub array (in)
+    :param end: the end position of the sub array (in)
+    :return: the sorted sub array
+    """
+    if start < end:
+        q = partition(array, start, end)
+        quicksort(array, start, q - 1)
+        quicksort(array, q + 1, end)
 
 
 if __name__ == "__main__":
-    t = np.random.rand(1000)
-    quicksort(1, 2, 3)
+    t = np.random.rand(100000)
+    t1 = time.clock()
+    quicksort(t, 0, len(t) - 1)
+    t2 = time.clock()
+    print(t2 - t1)
