eb0000232146f7f032b3d80de0d7495e136d7047
gdshen <gdshen95@gmail.com>
2015-04-07 15:18:34 +0800

    complete mergesort

--- exp1/mergesort.py
+++ exp1/mergesort.py
@@ -1,2 +1,42 @@
 __author__ = 'shen'
+import numpy as np
+import time
 
+
+def mergesort(array):
+    """
+    This is the mergesort.And it doesn't modify the array itself
+    :param array: unsorted array
+    :return: sorted array
+    """
+    n = len(array)
+    if n <= 1:
+        return array
+    a1 = array[:n // 2]
+    a2 = array[n // 2:]
+    a1 = mergesort(a1)
+    a2 = mergesort(a2)
+    return merge(a1, a2)
+
+
+def merge(a1, a2):
+    c = []
+    i, j = 0, 0
+    while i < len(a1) and j < len(a2):
+        if a1[i] <= a2[j]:
+            c.append(a1[i])
+            i += 1
+        else:
+            c.append(a2[j])
+            j += 1
+    if a1[i:]:
+        c += a1[i:]
+    if a2[j:]:
+        c += a2[j:]
+    return c
+
+
+if __name__ == "__main__":
+    l = np.random.rand(10)
+    li = l.tolist()
+    print(mergesort(li))
--- exp1/timetest.py
+++ exp1/timetest.py
@@ -1,2 +1,26 @@
 __author__ = 'shen'
+import numpy as np
+import time
+from exp1.quicksort import quicksort
+from exp1.bubblesort import bubblesort
+import arrow
 
+
+def getRandomArray(n):
+    """
+    Construct a random array for sorting
+    :param n: the number of elements in array
+    :return: a random array whose number of element is set by n
+    """
+    return np.random.rand(n)
+
+
+if __name__ == "__main__":
+    number = 1000, 5000, 10000
+    for i in number:
+        t1 = arrow.now()
+        array = getRandomArray(i)
+        quicksort(array, 0, len(array) - 1)
+        t2 = arrow.now()
+        t = t2 - t1
+        print("在{0}个数据下，{1}效率为{2} 秒".format(i, quicksort.__name__, t))
