e0c38c2a65ccc4f6c6f617452f133479179f53fc
gdshen <gdshen95@gmail.com>
2015-04-07 10:41:42 +0800

    complete bubble sort

--- exp1/bubblesort.py
+++ exp1/bubblesort.py
@@ -1,1 +1,26 @@
 __author__ = 'shen'
+import numpy as np
+import time
+
+
+def bubblesort(array):
+    """
+    This is the code for bubble sort algorithm.
+    :param array: the array to be sorted
+    :return: the sorted array
+    """
+    for i in range(len(array), 1, -1):
+        for j in range(i - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+    return array
+
+
+if __name__ == "__main__":
+    l = np.random.rand(10)
+    t1 = time.clock()
+    bubblesort(l)
+    t2 = time.clock()
+    print(l)
+    t = t2 - t1
+    print(t)
