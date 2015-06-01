363e69947e654a6c22ebd2540bad7501c034abe2
gdshen <gdshen95@gmail.com>
2015-06-01 23:02:01 +0800

    分支限界法完成

--- exp6/bag_cb.py
+++ exp6/bag_cb.py
@@ -16,8 +16,9 @@ class node:
     def __str__(self, *args, **kwargs):
         return "level{0} value{1} cw{2} cv{3}".format(self.level, self.value, self.cw, self.cv)
 
-w = [0, 5, 2, 1, 6, 3]
-v = [0, 3, 6, 8, 2, 1]
+
+w = [0, 1, 2, 5, 6, 3]
+v = [0, 8, 6, 3, 2, 1]
 c = 6
 n = len(w) - 1
 
@@ -28,17 +29,32 @@ def sack():
     best_x = 0
     d = deque()
     i = 1
+    up = bound(1, cw, cv, n)
     while i != n + 1:
         if cw + w[i] <= c:
             if cv + v[i] > best_x:
                 best_x = cv + v[i]
             d.append(node(i + 1, 1, cw + w[i], cv + v[i]))
-        d.append(node(i + 1, 0, cw, cv))
+        up = bound(i + 1, cw, cv, n)
+        if up > best_x:
+            d.append(node(i + 1, 0, cw, cv))
         p = d.popleft()
         i = p.level
         cv = p.cv
         cw = p.cw
     return best_x
+
+
+def bound(i, cw, cv, n):
+    cleft = c - cw
+    b = cv
+    while i <= n and w[i] <= cleft:
+        cleft -= w[i]
+        b += v[i]
+        i += 1
+    if i <= n:
+        b += v[i] / w[i] * cleft
+    return b
 
 
 if __name__ == '__main__':
